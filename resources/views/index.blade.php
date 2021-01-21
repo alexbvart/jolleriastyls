2c12cf3952d3a2261fc5f8372824b96af1566b17
Alexander Briones <alexanderbvart@gmail.com>
2021-01-21 09:38:46 -0500

    fix index login 4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="es">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es" xml:lang="es">
 <head>
-    <meta charset="utf-8" />
+    <meta http-equiv="content-type" charset="text/html; UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Jolleriastyls</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <style>
         :root{--breakpoint-xs: 0;--breakpoint-sm: 576px;--breakpoint-md: 768px;--breakpoint-lg: 992px;--breakpoint-xl: 1200px;--font-family-sans-serif: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";--font-family-monospace: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace}*,*::before,*::after{box-sizing:border-box}html{font-family:sans-serif;line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0;font-family:"Source Sans Pro",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";font-size:1rem;font-weight:400;line-height:1.5;color:#212529;text-align:left;background-color:#ffffff}h5{margin-top:0;margin-bottom:0.5rem}img{vertical-align:middle;border-style:none}label{display:inline-block;margin-bottom:0.5rem}button{border-radius:0}input,button{margin:0;font-family:inherit;font-size:inherit;line-height:inherit}button,input{overflow:visible}button{text-transform:none}button{-webkit-appearance:button}button::-moz-focus-inner{padding:0;border-style:none}::-webkit-file-upload-button{font:inherit;-webkit-appearance:button}h5{margin-bottom:0.5rem;font-family:inherit;font-weight:500;line-height:1.2;color:inherit}h5{font-size:1.25rem}.container{width:100%;padding-right:7.5px;padding-left:7.5px;margin-right:auto;margin-left:auto}@media (min-width:576px){.container{max-width:540px}}@media (min-width:768px){.container{max-width:720px}}@media (min-width:992px){.container{max-width:960px}}@media (min-width:1200px){.container{max-width:1140px}}@media (min-width:576px){.container{max-width:540px}}@media (min-width:768px){.container{max-width:720px}}@media (min-width:992px){.container{max-width:960px}}@media (min-width:1200px){.container{max-width:1140px}}.form-control{display:block;width:100%;height:calc(2.25rem + 2px);padding:0.375rem 0.75rem;font-size:1rem;font-weight:400;line-height:1.5;color:#495057;background-color:#ffffff;background-clip:padding-box;border:1px solid #ced4da;border-radius:0.25rem;box-shadow:inset 0 0 0 rgba(0,0,0,0)}.form-control::-ms-expand{background-color:transparent;border:0}.form-control:-moz-focusring{color:transparent;text-shadow:0 0 0 #495057}.form-control::-webkit-input-placeholder{color:#939ba2;opacity:1}.form-control::-moz-placeholder{color:#939ba2;opacity:1}.form-control:-ms-input-placeholder{color:#939ba2;opacity:1}.form-control::-ms-input-placeholder{color:#939ba2;opacity:1}.form-group{margin-bottom:1rem}.btn{display:inline-block;font-weight:400;color:#212529;text-align:center;vertical-align:middle;background-color:transparent;border:1px solid transparent;padding:0.375rem 0.75rem;font-size:1rem;line-height:1.5;border-radius:0.25rem}.btn-success{color:#ffffff;background-color:#28a745;border-color:#28a745;box-shadow:none}.btn-lg{padding:0.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:0.3rem}.btn-block{display:block;width:100%}.input-group{position:relative;display:-ms-flexbox;display:flex;-ms-flex-wrap:wrap;flex-wrap:wrap;-ms-flex-align:stretch;align-items:stretch;width:100%}.input-group>.form-control{position:relative;-ms-flex:1 1 0%;flex:1 1 0%;min-width:0;margin-bottom:0}.input-group>.form-control:not(:first-child){border-top-left-radius:0;border-bottom-left-radius:0}.input-group-prepend{display:-ms-flexbox;display:flex}.input-group-prepend{margin-right:-1px}.input-group-text{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;padding:0.375rem 0.75rem;margin-bottom:0;font-size:1rem;font-weight:400;line-height:1.5;color:#495057;text-align:center;white-space:nowrap;background-color:#e9ecef;border:1px solid #ced4da;border-radius:0.25rem}.input-group>.input-group-prepend>.input-group-text{border-top-right-radius:0;border-bottom-right-radius:0}.card{position:relative;display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;min-width:0;word-wrap:break-word;background-color:#ffffff;background-clip:border-box;border:0 solid rgba(0,0,0,0.125);border-radius:0.25rem}.card-body{-ms-flex:1 1 auto;flex:1 1 auto;min-height:1px;padding:1.25rem}.card-header{padding:0.75rem 1.25rem;margin-bottom:0;background-color:rgba(0,0,0,0.03);border-bottom:0 solid rgba(0,0,0,0.125)}.card-header:first-child{border-radius:calc(0.25rem - 0) calc(0.25rem - 0) 0 0}.card-footer{padding:0.75rem 1.25rem;background-color:rgba(0,0,0,0.03);border-top:0 solid rgba(0,0,0,0.125)}.card-footer:last-child{border-radius:0 0 calc(0.25rem - 0) calc(0.25rem - 0)}.float-left{float:left!important}.mt-4{margin-top:1.5rem!important}.my-auto{margin-top:auto!important}.my-auto{margin-bottom:auto!important}.text-center{text-align:center!important}.text-white{color:#ffffff!important}.text-muted{color:#6c757d!important}html,body{min-height:100%}label:not(.form-check-label):not(.custom-file-label){font-weight:700}.card{box-shadow:0 0 1px rgba(0,0,0,0.125),0 1px 3px rgba(0,0,0,0.2);margin-bottom:1rem}.card-header::after,.card-body::after,.card-footer::after{display:block;clear:both;content:""}.card-header{background-color:transparent;border-bottom:1px solid rgba(0,0,0,0.125);padding:0.75rem 1.25rem;position:relative;border-top-left-radius:0.25rem;border-top-right-radius:0.25rem}.card-text{clear:both}.text-white{color:#ffffff!important}.bg-black{background-color:#000;color:#ffffff!important}
     </style>
@@ -18,7 +18,7 @@
             @csrf
             @method('POST')
             <h2  name="inicio de sesiòn" class="form-title">Inicio de Sesión</h2>
-            <div class="form-group">
+            <div class="form-group mt-4">
                 <label class="control-label" for="name">Nombre:</label>
                 <div class="input-icon">
                     <input class="form-control @error('name') is-invalid @enderror" type="text" title="Nombre del usuario"
@@ -42,11 +42,11 @@
                     @enderror
                 </div>
             </div>
-            <hr />
+
             <div class="form-actions mt-4">
                 <input  class="btn btn-success btn-block btn-lg" type="submit"value="Ingresar" />
             </div>
-            <hr />
+
         </form>
     </div>
     <div class="copyright mt-2">
@@ -59,11 +59,6 @@
     
     <script src="/adminlte/plugins/jquery/jquery.min.js"></script>
     <script src="/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    
-    {{--     <script src="/adminlte/dist/js/adminlte.min.js"></script> --}}
-    
-    {{-- <link rel="stylesheet" href="/adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"> --}}
-    {{--     <link rel="stylesheet" href="/adminlte/plugins/fontawesome-free/css/all.min.css"> --}}
     <link rel="stylesheet" href="/adminlte/dist/css/adminlte.min.css"> 
 
 
